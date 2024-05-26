bc981a1cc764cd798c00fbc7db8c995a769a0d42
JOSHIK <joshikroshan4021@gmail.com>
2024-05-26 09:18:13 +0100

    auth finshed

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,22 +1,28 @@
 import { Button } from "@/components/ui/button";
-
 import { Input } from "@/components/ui/input";
-
 import { signIn } from "../auth";
 
 export default function LoginCard() {
   return (
-    <form
-      action={async (formData) => {
-        "use server";
-        await signIn("resend", formData);
-      }}
-      className="space-y-[8px] mt-12 w-[500px] mx-auto  p-12 rounded-lg shadow-xl"
-    >
-      <Input type="text" name="email" placeholder="Email" />
-      <Button className="w-full bg-[#315196] hover:bg-[#2d4069]" type="submit">
-        Sign In
-      </Button>
-    </form>
+    <div className="mt-12 w-[500px] mx-auto p-12 rounded-lg shadow-xl text-center">
+      <div className="flex justify-center items-center mb-4">
+        <div className="font-[600] text-[28px] text-[#315196]">Login</div>
+      </div>
+      <form
+        action={async (formData) => {
+          "use server";
+          await signIn("resend", formData);
+        }}
+        className="space-y-[8px] "
+      >
+        <Input type="text" name="email" placeholder="Email" />
+        <Button
+          className="w-full bg-[#315196] hover:bg-[#2d4069]"
+          type="submit"
+        >
+          Sign In With Email
+        </Button>
+      </form>
+    </div>
   );
 }
