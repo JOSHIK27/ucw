7ebcf6c740717d94c518dedb1d2e51718ac92fa0
JOSHIK <joshikroshan4021@gmail.com>
2024-05-30 11:30:02 +0100

    types bugs

--- src/app/(pages)/post/page.tsx
+++ src/app/(pages)/post/page.tsx
@@ -1,15 +1,24 @@
 import MultiForm from "@/components/multiForm";
 import { supabase } from "@/lib/supabase";
+import { PostgrestSingleResponse } from "@supabase/supabase-js";
 export default async function Page() {
-  const resp1 = await supabase.from("university").select();
-  const universityList = resp1?.data?.map((item) => item.name);
+  const resp = await supabase.from("university").select();
+  const universityList = resp?.data?.map((item) => item.name);
 
-  const { data } = await supabase.from("course").select();
-
+  type coursePropItem = {
+    id: Number;
+    name: string;
+    prof_id: Number;
+    university: string;
+  };
+  const { data }: PostgrestSingleResponse<coursePropItem[]> = await supabase
+    .from("course")
+    .select();
+  console.log(data);
   return (
     <MultiForm
       universityList={universityList ? universityList : []}
-      courseList={data}
+      courseList={data ? data : []}
     />
   );
 }
